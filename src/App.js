2eedb6746d0e40858ed52dd47e8d56f4803a043e
Paul Yorde <paulyorde26@gmail.com>
2022-03-26 10:32:30 -0400

    tone up

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import * as Tone from 'tone';
+
+const synth = new Tone.MembraneSynth().toDestination();
+
+function playSynth() {
+  synth.triggerAttackRelease("C2", "8n");
+}
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <div id='wrapper'>
+       <button id='button' onClick={playSynth}>play</button>
+       </div>
       </header>
     </div>
   );
