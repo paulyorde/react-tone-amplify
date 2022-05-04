46adc1bfe4cadeccce98d3639abc4a4043397e87
Paul Yorde <paulyorde26@gmail.com>
2022-05-04 08:40:20 -0400

    pingpong to effectbox

--- src/tone-components/effects/effect-box.js
+++ src/tone-components/effects/effect-box.js
@@ -1,21 +1,37 @@
+import { useState } from "react";
+import PingPong from "./pingpong/pingpont.component";
+
+import { IoOptionsOutline } from "react-icons/io5";
 
 
 const EffectBox = () => {
 
-    // const [showEffectBox, setShowEffectBox] = useState('')
+    let [getEffects, setEffects] = useState([])
+    let [trackKey, setTrackKey] = useState(0)
+    /** build effects component */
+    // const openEffects = () => {
+    //     const  keyCounter = trackKey + 1
+    //     setTrackKey(keyCounter)
+    //     setEffects(<PingPong key={keyCounter} />)
+    //   }
 
-    // const toggleEffects = () => {
-    //     // hide div
-    //     // showEffectBox = !showEffectBox
-    //     setShowEffectBox(showEffectBox = !showEffectBox)
-    // }
-
+    const openEffects = () => {
+        setTrackKey(trackKey + 1)
+        const newEffect = [<PingPong key={trackKey} />]
+        setEffects(newEffect)
+      }
 
     return (
         <>
         <div className="effect-box">
-            {/* <button onClick={toggleEffects} className="effect-close">-</button>
-            {showEffectBox ? <EffectBox /> : null} */}
+            {/* list of effects */}
+            {/* show hide effects -> effect params within effect */} 
+            {getEffects}
+            <button 
+                onClick={openEffects} 
+                className='pong tooltip'><IoOptionsOutline/>
+                <span className='tooltiptext'>open effects</span>
+            </button>
         </div>
         </>
     )
--- src/tone-components/track/track.component.js
+++ src/tone-components/track/track.component.js
@@ -1,62 +1,36 @@
 import Player from "../player/player.component";
 import Recorder from "../recorder/recorder.component"
 import EffectBox from '../../tone-components/effects/effect-box';
-// import PingPong from '../../tone-components/effects/pingpong/pingpont.component';
 
 import { IoOptionsOutline } from "react-icons/io5";
 
 import { useState } from "react";
 
 const Track = () => {
-   
-    let [getEffects, setEffects] = useState([])
-    let [trackKey, setTrackKey] = useState(0)
-    {/* pass trackkey=#  to recorder / effects to display */}
 
-    /** build effects component */
-    // const openEffects = () => {
-    //     const  keyCounter = trackKey + 1
-    //     setTrackKey(keyCounter)
-    //     setEffects(<PingPong key={keyCounter} />)
-    //   }
+    let [showEffectBox, setShowEffectBox] = useState('')
 
-      const openEffects = () => {
-        const  keyCounter = trackKey + 1
-        const newEffect = [<EffectBox />]
-        setEffects(newEffect)
-      }
-
-      let [showEffectBox, setShowEffectBox] = useState('')
-
-      const toggleEffects = () => {
-          // hide div
-          // showEffectBox = !showEffectBox
-          setShowEffectBox(showEffectBox = !showEffectBox)
-      }
-
-    //   const closeEffects = () => {
-
-    //   }
+    const toggleEffects = () => {
+        setShowEffectBox(showEffectBox = !showEffectBox)
+    }
 
     return (
         <>
-            <div className="track">
-                <Recorder />
+        <div className="track">
+            <Recorder />
+            
+            <button onClick={toggleEffects} className="pong tooltip"><IoOptionsOutline/></button>
+            {showEffectBox ? <EffectBox className="effect-box"/> : null}
+            
 
-                {/* {getEffects}
-                <button onClick={openEffects} className='pong tooltip'><IoOptionsOutline/><span className='tooltiptext'>open effects</span>---</button> */}
-                <button onClick={toggleEffects} className="effect-close pong tooltip"><IoOptionsOutline/></button>
-                {showEffectBox ? <EffectBox /> : null}
-                {/* <button onClick={closeEffects} className="effect-close">-</button> */}
-
-                {/* Replace with <Effects />
-                <div className="effectSoundBox">
-                    {effects}
-                </div> */}
-                {/* <button onClick={openEffects} className='pong tooltip'><IoOptionsOutline/><span className='tooltiptext'>open effects</span></button> */}
-                
-                <Player />
-            </div>
+            {/* Replace with <Effects />
+            <div className="effectSoundBox">
+                {effects}
+            </div> */}
+            {/* <button onClick={openEffects} className='pong tooltip'><IoOptionsOutline/><span className='tooltiptext'>open effects</span></button> */}
+            
+            <Player />
+        </div>
         </>
     )
 }
